78dc6cf1a05e4846514019ce82ba2820ea4f3f33
Jimmy Kim <jimmykim9001@gmail.com>
2017-10-23 15:29:07 -0700

    Updated to reflect costaricacsv

--- plot.js
+++ plot.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $.ajax({
     type: "GET",
-    url: "Korea_Fertility_converted.csv",
+    url: "costarica.csv",
     dataType: "text",
     success: function(data){plot(data);}
   });
